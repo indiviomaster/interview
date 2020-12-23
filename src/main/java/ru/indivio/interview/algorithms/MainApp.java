b8416d6fb6783c87653890a0833eb786b421b81f
indivioM <indivio@yandex.ru>
2020-12-23 21:28:25 +0300

    Lesson 2 ArrayList

--- src/main/java/ru/indivio/interview/algorithms/MainApp.java
+++ src/main/java/ru/indivio/interview/algorithms/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
 
 
     public static void main(String[] args) {
-        LList<String> list = new LList<>();
+        /*LList<String> list = new LList<>();
         long time =System.currentTimeMillis();
         for(int i=0;i<1000000;i++){
             list.add("data+".concat(String.valueOf(i)));
@@ -19,6 +19,20 @@ public class MainApp {
             System.out.println(list.remove("data+".concat(String.valueOf(i))));
 
         }
-        System.out.println(list.size());
+        System.out.println(list.size());*/
+        AList<String> alist = new AList<>();
+        alist.add("A");
+        alist.add("B");
+        alist.add("C");
+        alist.add("D");
+        alist.add("E");
+        alist.set(0,"L");
+        alist.remove(3);
+        /*for (int i = 0; i < 4000000; i++) { //установлен предел 400 элементов
+            alist.add("Z");
+        }*/
+        System.out.println(alist.size());
+        System.out.println(alist.get(3));
+        System.out.println(alist);
     }
 }
